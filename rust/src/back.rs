204dc65435b6f730dd8c83e3341fda04a2300edf
Mehmet Emre <memre@usfca.edu>
2026-09-01 13:58:19 -0700

    add the license info

--- rust/src/back.rs
+++ rust/src/back.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2026 Mehmet Emre
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// This file is part of smol.
+//
+// smol is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License version 3 as published by the Free
+// Software Foundation.
+//
+// smol is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// smol. If not, see <https://www.gnu.org/licenses/>.
+
 //! The back-end of the compiler.
 
 pub mod asm;
